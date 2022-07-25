69e25016f28cbd806c6da33443b5727c4daa4207
Angel Garcia <angelgarciaweb@gmail.com>
2022-07-25 07:02:08 -0400

    Blur

---
+++ blur.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+
+pic = cv2.imread('spiderman.jpeg', 0)
+
+matrix = (7,7)
+
+blur = cv2.GaussianBlur(pic, matrix, 0)
+
+cv2.imshow('Binary', blur)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
